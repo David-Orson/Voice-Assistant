f6c53ba03f09030bc70a08349b7af9d833b5685a
David Orson <onlyorson@outlook.com>
2020-04-19 03:08:30 +0100

    Continuos Listening, Parrot Functionality

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,52 @@
-const btn = document.querySelector(".talk");
-const speechToText = document.querySelector(".speech-to-text");
+const btn: any = document.querySelector(".talk");
+const speechToText: any = document.querySelector(".speech-to-text");
+
+const MySpeechRecognition = window.webkitSpeechRecognition;
+const recognition = new MySpeechRecognition();
+
+let voices: any;
+
+function setVoice() {
+  voices = window.speechSynthesis.getVoices();
+}
+
+setVoice();
+
+recognition.onstart = function () {
+  console.log("listening");
+  setTimeout(() => {
+    recognition.start();
+  }, 6000);
+};
+
+recognition.onresult = function (event: any) {
+  const current = event.resultIndex;
+
+  const transcript = event.results[current][0].transcript;
+  speechToText.textContent = transcript;
+  readOutLoud(transcript);
+};
+
+//listener
+
+btn.addEventListener("click", () => {
+  recognition.start();
+});
+
+function readOutLoud(message: any) {
+  voices = window.speechSynthesis.getVoices();
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 2;
+
+  speech.voice = voices[2];
+  console.log(voices);
+
+  window.speechSynthesis.speak(speech);
+  setTimeout(() => {
+    recognition.start();
+  }, 1000);
+}
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,3 +1,40 @@
 "use strict";
 var btn = document.querySelector(".talk");
 var speechToText = document.querySelector(".speech-to-text");
+var MySpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new MySpeechRecognition();
+var voices;
+function setVoice() {
+    voices = window.speechSynthesis.getVoices();
+}
+setVoice();
+recognition.onstart = function () {
+    console.log("listening");
+    setTimeout(function () {
+        recognition.start();
+    }, 6000);
+};
+recognition.onresult = function (event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    speechToText.textContent = transcript;
+    readOutLoud(transcript);
+};
+//listener
+btn.addEventListener("click", function () {
+    recognition.start();
+});
+function readOutLoud(message) {
+    voices = window.speechSynthesis.getVoices();
+    var speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 2;
+    speech.voice = voices[2];
+    console.log(voices);
+    window.speechSynthesis.speak(speech);
+    setTimeout(function () {
+        recognition.start();
+    }, 1000);
+}
